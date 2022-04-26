7b75e710a57a10458bf4755c7254038b8853d8bd
davidkrushnisky <91700482+davidkrushnisky@users.noreply.github.com>
2022-04-26 12:55:58 -0400

    fiboCache

--- src/session4/FiboCache.java
+++ src/session4/FiboCache.java
@@ -1,19 +1,15 @@
 package session4;
 
+import org.junit.Assert;
+import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FiboCache {
 
-    public static void main(String[] args) {
-        fiboCache();
-        System.out.println(getNthFib(45));
-        System.out.println(getCountOfFibsComputed());
-        System.out.println(getFibsCached());
-    }
-
-    static private List<Long> fibsCached = new ArrayList<>(); // NOT a HashMap !
+    static private List<Long> fibsCached = new ArrayList<>();
     static private int fibsCompCount = 2;
+    static private int fibsCachedCount = 0;
 
     static public void fiboCache() {
         fibsCached.add(0L);
@@ -22,6 +18,7 @@ public class FiboCache {
 
     static public long getNthFib(int n) {
         if (n < fibsCached.size()){
+            fibsCachedCount++;
             return fibsCached.get(n);
         }
         long result = getNthFib(n - 1) + getNthFib(n -2);
@@ -34,10 +31,33 @@ public class FiboCache {
         return fibsCompCount;
     }
 
+    static public int getCountOfFibsCachedUsed() {
+        return fibsCachedCount;
+    }
+
     static public List<Long> getFibsCached() {
         return fibsCached;
     }
 
     //Create Unit test to prove the method
+    @Test
+    public void testGet7thFib(){
+        FiboCache.fiboCache();
+        Assert.assertEquals(13, getNthFib(7));
+    }
 
+    @Test
+    public void testGet19thFib(){
+        FiboCache.fiboCache();
+        Assert.assertEquals(4181, getNthFib(19));
+    }
+
+    public static void main(String[] args) {
+        FiboCache.fiboCache();
+        System.out.println("The Fibonacci number is: " + getNthFib(15));
+        System.out.println("The count of fibs computed is: " + getCountOfFibsComputed());
+        System.out.println("The count of fibs cached used is: " + FiboCache.getCountOfFibsCachedUsed());
+        System.out.println("The fibs cached are: " + FiboCache.getFibsCached());
+    }
 }
+
